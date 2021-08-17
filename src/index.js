32fdfc6825a486997d49f600fe90e1b48a20cd88
TatsiaA <taniokk@mail.ru>
2021-08-18 00:58:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+    let str = String(n);
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+
+    } else {
+        n = - n;
+        return reverse(n);
+    }
+    
 }
